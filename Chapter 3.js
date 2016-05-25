13476f8e09983ef1d6b68b7c7fa218d4e75106c4
archcorsair <archcorsair@gmail.com>
2016-05-25 14:20:44 -0700

    Fixed file name typo, moved chp3 work from chp4 file

--- Chapter 3.js
+++ Chapter 3.js
@@ -28,3 +28,37 @@ function isEven(input) {
 console.log(isEven(50)); //returns true
 console.log(isEven(75)); //returns false
 console.log(isEven(-1)); //returns false
+
+/*
+** Write a function countBs that takes a string as its only argument and
+** returns a number that indicates how many uppercase "B" characters are in the string.
+*/
+
+function countBs(input) {
+  let bCount = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input.charAt(i) === 'B') {
+      bCount++;
+    }
+  }
+  return bCount;
+}
+
+console.log(countBs('But Bob, why is the boy so badly behaved?')); // returns 2
+
+/*
+** Next write a function called countChar that behaves like countBs, except it
+** takes a second argument that indicates the chatacter that is to be counted
+*/
+
+function countChar(input, char) {
+  let charCount = 0;
+  for (let i = 0; i < input.length; i++) {
+    if (input.charAt(i) === char) {
+      charCount++;
+    }
+  }
+  return charCount;
+}
+
+console.log(countChar('CNN is a source for world news.', 'N')); // returns 2
