ed3d38476e1f8a881e8a6048a9f145e49e3b3d51
archcorsair <archcorsair@gmail.com>
2016-05-25 18:10:39 -0700

    Added chapter 4 - for real this time

--- Chapter 4.js
+++ Chapter 4.js
@@ -1,1 +1,15 @@
+/*
+** How to compute phi coefficient by example
+*/
 
+function phi(table) {
+  return (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt((table[2] + table[3]) *
+              (table[0] + table[1]) *
+              (table[1] + table[3]) *
+              (table[0] + table[2]));
+}
+
+console.log(phi([76, 9, 4, 1]));
+
+// Function to remove a specific element at a given index
