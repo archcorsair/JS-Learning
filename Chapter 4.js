d1187aa604943f50bc2f4a385ba3846b3e68f986
archcorsair <archcorsair@gmail.com>
2016-05-26 17:52:58 -0700

    Continued work on Chp4

--- Chapter 4.js
+++ Chapter 4.js
@@ -1,6 +1,4 @@
-/*
-** How to compute phi coefficient by example
-*/
+// How to compute phi coefficient by example
 
 function phi(table) {
   return (table[3] * table[0] - table[2] * table[1]) /
@@ -9,7 +7,38 @@ function phi(table) {
               (table[1] + table[3]) *
               (table[0] + table[2]));
 }
-
 console.log(phi([76, 9, 4, 1]));
 
-// Function to remove a specific element at a given index
+// Write a range function that takes two arguments, start and end, and returns
+// an array containing all the numbers from start up to (and including) end.
+
+const numbers = []; // called outside for sum function access
+function range(start, end) {
+  for (let i = start; i <= end; i++) {
+    numbers.push(i);
+  }
+  return numbers;
+}
+console.log(range(1, 10)); // returns [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// Next write a sum function that takes an array of numbers and returns the
+// sum of these numbers.
+
+function sum(array) {
+  let sumOf = 0;
+  for (let i = 0; i < array.length; i++) {
+    sumOf += array[i];
+  }
+  return sumOf;
+}
+console.log(sum(numbers));
+
+// Modify the range function to take an optional third argument that indicates
+// the "step" value used to build up the array. If no step is given, the array
+// elements go up by increments of one. Make sure it also works with negative
+// step values.
+
+// function bonusRange(start, end, step) {
+//
+// }
+// work in progress
